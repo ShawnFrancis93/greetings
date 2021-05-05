c68953755421c95e7b111fbd9611f8e0136f7abf
Shawn <shawnpfrancis1993@gmail.com>
2021-05-05 19:54:11 -0400

    greetings

---
+++ Greeting.py
@@ -0,0 +1,8 @@
+Name = (input("What do I call ya?"))
+print ("I will call you ", Name)
+if Name == 'Shawn':
+    print ("Hey, I'm Shawn too.")
+
+for i in range(10):
+    [input('Please give me your name: ')]
+print ("It was nice to meet you all")
